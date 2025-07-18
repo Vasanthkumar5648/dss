d5a2512b8dceb0e080f2dd378dec4b9bdb9db3fc
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-07-18 10:19:10 +0530

    Update sd.py

--- sd.py
+++ sd.py
@@ -1,4 +1,4 @@
 import streamlit as st
-from tensorflow.keras.models import Sequential
+from tensorflow as tf
 st.title("🍔 Food Delivery Time Prediction")
 st.markdown("Predict how long your food delivery will take based on delivery partner details and distance.")
