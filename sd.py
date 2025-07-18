5b9007769e80dabaaa0a61f3c507d5e7979e6b51
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-07-18 09:51:13 +0530

    Create sd.py

---
+++ sd.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from tensorflow import Sequential
+st.title("🍔 Food Delivery Time Prediction")
+st.markdown("Predict how long your food delivery will take based on delivery partner details and distance.")
